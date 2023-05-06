b801356b5bbe5e241e40efac3000828ebf42fd7b
Lex010 <lex07672@gmail.com>
2023-05-06 16:43:57 +0300

    05

--- for_script.js
+++ for_script.js
@@ -4,6 +4,15 @@ const email = document.getElementById('email')
 const adress = document.getElementById('adress')
 
 const services = document.getElementById('first_sub')
+//привязка для замены текста при смене языка
+const enRuUa = document.getElementById('ru_en_ua')
+const ua = document.getElementById('UA')
+
+const fs1 = document.getElementById('fs_1')
+const fs2 = document.getElementById('fs_2')
+const fs3 = document.getElementById('fs_3')
+const fs4 = document.getElementById('fs_4')
+
 //       //       //         //          //
 //
 function backToBisqit () {       
@@ -28,3 +37,16 @@ buttonContact[0].onclick = function() {
     setTimeout(backToBisqitServ,500)
 }
 //
+//код для замены текста при смене языка
+ua.onclick = function() {
+    buttonContact[0].textContent = 'Наші послуги'
+    buttonContact[1].textContent = 'Контакти'
+    enRuUa.textContent = 'Укр'
+
+     fs1.textContent = 'Детективні послуги.'
+     fs2.textContent = 'Безпека бізнезу.'
+     fs3.textContent = 'Приватна охорона.'
+     fs4.textContent = 'Допомога у вирішенні конфліктних ситуацій.'
+    //services.textContent = '- Детективні послуги. /b- Безпека бізнезу. <br>- Приватна охорона. <br>- Допомога у вирішенні конфліктних ситуацій.'
+
+}
